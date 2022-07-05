437b054a89823b24f921ef72f458746a283cd97a
ahmad2743 <amethdione10@gmail.com>
2022-07-05 14:26:56 +0200

    initialize project

---
+++ devSocialNetwork/devSocialNetwork/devSocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  devSocialNetworkApp.swift
+//  devSocialNetwork
+//
+//  Created by Ahamad Ben on 05/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct devSocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
